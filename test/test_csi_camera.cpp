118f67617918d655750a3db794523b5ac11b7e7d
seanavery <seanpollock3344@gmail.com>
2023-07-10 17:19:23 -0400

    Test csi_camera init

--- test/test_csi_camera.cpp
+++ test/test_csi_camera.cpp
@@ -6,4 +6,10 @@
 // Test that the camera can be created and destroyed
 TEST(CSICamera, CreateEmpty) {
     CSICamera camera("test", {});
+
+    EXPECT_EQ(camera.is_debug(), false);
+    EXPECT_EQ(camera.get_width_px(), DEFAULT_INPUT_WIDTH);
+    EXPECT_EQ(camera.get_height_px(), DEFAULT_INPUT_HEIGHT);
+    EXPECT_EQ(camera.get_frame_rate(), DEFAULT_INPUT_FRAMERATE);
+    EXPECT_EQ(camera.get_video_path(), DEFAULT_INPUT_SENSOR);
 }
