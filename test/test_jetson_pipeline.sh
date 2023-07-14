350998affa1ba682cf554887f986d495b206a9fb
seanavery <seanpollock3344@gmail.com>
2023-07-14 16:31:11 -0400

    jetson pipeline shell test

---
+++ test/test_jetson_pipeline.sh
@@ -0,0 +1,1 @@
+gst-launch-1.0 nvarguscamerasrc sensor_id=0 ! video/x-raw(memory:NVMM),width=1640,height=1232,framerate=30/1 ! nvvidconv flip-method=0 ! video/x-raw,width=960,height=540 ! nvjpegenc ! image/jpeg ! appsink name=appsink0 max-buffers=1
